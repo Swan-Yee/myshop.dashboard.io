6a099bd7d2739fc2ad514dc62cddb80d64d93576
Swan Yee Tun <swanyeetun.2000@gmail.com>
2021-06-15 23:41:21 +0630

    final stage of DB Designn

--- js/app.js
+++ js/app.js
@@ -6,3 +6,25 @@ $(".show-side-bar-btn").click(function () {
 $(".hide-side-bar-btn").click(function () {
     $(".aside").animate({marginLeft: "-100%" });
 })
+
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+// fullscreen
+
+$('.full-screen').click(function () {
+  let current=$(this).closest(".card")
+  $(this).closest(".card").toggleClass('full-screen-card');
+  if (current.hasClass("full-screen-card")) {
+    $(this).html(`<i class="feather-minimize-2"></i>`);
+  }
+  else {
+    $(this).html(`<i class="feather-maximize-2"></i>`)
+  }
+})
+
+$(document).ready(function() {
+    $('#list').DataTable();
+} );
