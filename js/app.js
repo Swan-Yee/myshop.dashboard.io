7cadb3066651cf5414330f46c18423d85db9f2e1
Swan Yee Tun <swanyeetun.2000@gmail.com>
2021-06-16 16:15:23 +0630

    add loading

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+var animation = bodymovin.loadAnimation({
+            // animationData: { /* ... */ },
+            container: document.getElementById('loading-container'), // required
+            path: 'https://assets2.lottiefiles.com/packages/lf20_y9dfylgv.json', // required
+            renderer: 'svg', // required
+            loop: true, // optional
+            autoplay: true, // optional
+            name: "Demo Animation", // optional
+        });
+
+$(window).on("load", function () {
+    $('.loader-container').fadeOut(1000,function () {
+        $(this).remove();
+    });
+});
+
 // To show menu layout when ph screen
 $(".show-side-bar-btn").click(function () {
     $(".aside").animate({marginLeft: "0"});
@@ -7,13 +23,11 @@ $(".hide-side-bar-btn").click(function () {
     $(".aside").animate({marginLeft: "-100%" });
 })
 
-
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
 
 // fullscreen
-
 $('.full-screen').click(function () {
   let current=$(this).closest(".card")
   $(this).closest(".card").toggleClass('full-screen-card');
@@ -24,7 +38,3 @@ $('.full-screen').click(function () {
     $(this).html(`<i class="feather-maximize-2"></i>`)
   }
 })
-
-$(document).ready(function() {
-    $('#list').DataTable();
-} );
